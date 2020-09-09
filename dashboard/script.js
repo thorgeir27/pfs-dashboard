798837311e5f66a701f01ced7a4d2464f0bc26cd
Þorgeir <thorgeir93@gmail.com>
2020-09-09 15:57:57 +0000

    add all data

--- dashboard/script.js
+++ dashboard/script.js
@@ -40,37 +40,49 @@ function sveitarfelog(event) {
     let svfSelected = $("#val_sveitarfelag")[0];
     let gognSelected = $("#val_gogn")[0];
 
-    if (event.target.id == 'allt') {
-        svfSelected[1].checked = false
-        for (let i = 2;i < svfSelected.length; i++) {
-            svfSelected[i].checked = true;
+    if (event != null) {
+        if (event.target.id == 'allt') {
+            svfSelected[1].checked = false
+            for (let i = 2;i < svfSelected.length; i++) {
+                svfSelected[i].checked = true;
+            }
+        } else if (event.target.id == 'ekkert') {
+            svfSelected[0].checked = false
+            for (let i = 0;i < svfSelected.length; i++) {
+                svfSelected[i].checked = false;
+            }
+            svfSelected[1].checked = true;
+        } else {
+            svfSelected[0].checked = false;
+            svfSelected[1].checked = false;
         }
-    } else if (event.target.id == 'ekkert') {
-        svfSelected[0].checked = false
-        for (let i = 0;i < svfSelected.length; i++) {
-            svfSelected[i].checked = false;
-        }
-        svfSelected[1].checked = true;
-    } else {
-        svfSelected[0].checked = false;
-        svfSelected[1].checked = false;
     }
 
-    let data = [
+    let fastData = [
         new Trace('30 Mb/s', blue),
         new Trace('100 Mb/s', green),
-        new Trace('1000 Mb/s', mustard),
+        new Trace('1 Gb/s', mustard),
         new Trace('Ljósleiðari', red),
         new Trace('30 Mb/sT', blueT),
         new Trace('30 Mb/sD', blueD),
         new Trace('100 Mb/sT', greenT),
         new Trace('100 Mb/sD', greenD),
-        new Trace('1000 Mb/sT', mustardT),
-        new Trace('1000 Mb/sF', mustardD),
+        new Trace('1 Gb/sT', mustardT),
+        new Trace('1 Gb/sF', mustardD),
         new Trace('LjósleiðariT', redT),
         new Trace('LjósleiðariD', redD)
         
     ];
+
+    let farData = [
+        new Trace('Talsamband', blue),
+        new Trace('Háhraðafarnet', green),
+        new Trace('Talsamband í þéttbýli', blueT),
+        new Trace('Talsamband í dreifbýli', blueD),
+        new Trace('Háhraðafarnet í þéttbýli', greenT),
+        new Trace('Háhraðafarnet í dreifbýli', greenD),
+    ]
+
 
     let svf;
     let svfComp;
@@ -81,45 +93,65 @@ function sveitarfelog(event) {
 
             if (gognSelected[6].checked) {
                 if (gognSelected[0].checked) {
-                    data[4].x.push(svf);
-                    data[4].y.push((svfComp.t_30/svfComp.thettbyli_fjoldi)*100);
-                    data[5].x.push(svf);
-                    data[5].y.push((svfComp.d_30/svfComp.dreifbyli_fjoldi)*100);
+                    fastData[4].x.push(svf);
+                    fastData[4].y.push((svfComp.t_30/svfComp.thettbyli_fjoldi)*100);
+                    fastData[5].x.push(svf);
+                    fastData[5].y.push((svfComp.d_30/svfComp.dreifbyli_fjoldi)*100);
                 }
                 if (gognSelected[1].checked) {
-                    data[6].x.push(svf);
-                    data[6].y.push((svfComp.t_100/svfComp.thettbyli_fjoldi)*100);
-                    data[7].x.push(svf);
-                    data[7].y.push((svfComp.d_100/svfComp.dreifbyli_fjoldi)*100);
+                    fastData[6].x.push(svf);
+                    fastData[6].y.push((svfComp.t_100/svfComp.thettbyli_fjoldi)*100);
+                    fastData[7].x.push(svf);
+                    fastData[7].y.push((svfComp.d_100/svfComp.dreifbyli_fjoldi)*100);
                     }
                 if (gognSelected[2].checked) {
-                    data[8].x.push(svf);
-                    data[8].y.push((svfComp.t_1000/svfComp.thettbyli_fjoldi)*100);
-                    data[9].x.push(svf);
-                    data[9].y.push((svfComp.d_1000/svfComp.dreifbyli_fjoldi)*100);
+                    fastData[8].x.push(svf);
+                    fastData[8].y.push((svfComp.t_1000/svfComp.thettbyli_fjoldi)*100);
+                    fastData[9].x.push(svf);
+                    fastData[9].y.push((svfComp.d_1000/svfComp.dreifbyli_fjoldi)*100);
                 }
                 if (gognSelected[3].checked) {
-                    data[10].x.push(svf);
-                    data[10].y.push((svfComp.t_ljos/svfComp.thettbyli_fjoldi)*100);
-                    data[11].x.push(svf);
-                    data[11].y.push((svfComp.d_ljos/svfComp.dreifbyli_fjoldi)*100);
+                    fastData[10].x.push(svf);
+                    fastData[10].y.push((svfComp.t_ljos/svfComp.thettbyli_fjoldi)*100);
+                    fastData[11].x.push(svf);
+                    fastData[11].y.push((svfComp.d_ljos/svfComp.dreifbyli_fjoldi)*100);
+                }
+                if (gognSelected[4].checked) {
+                    farData[2].x.push(svf);
+                    farData[2].y.push((svfComp.t_talsamband/svfComp.thettbyli_fjoldi)*100);
+                    farData[3].x.push(svf);
+                    farData[3].y.push((svfComp.d_talsamband/svfComp.dreifbyli_fjoldi)*100);
+                }
+                if (gognSelected[5].checked) {
+                    farData[4].x.push(svf);
+                    farData[4].y.push((svfComp.t_hahrada/svfComp.thettbyli_fjoldi)*100);
+                    farData[5].x.push(svf);
+                    farData[5].y.push((svfComp.d_hahrada/svfComp.dreifbyli_fjoldi)*100);
                 }
             } else {
                 if (gognSelected[0].checked) {
-                    data[0].x.push(svf);
-                    data[0].y.push((svfComp.d_30 + svfComp.t_30)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
+                    fastData[0].x.push(svf);
+                    fastData[0].y.push((svfComp.d_30 + svfComp.t_30)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
                 }
                 if (gognSelected[1].checked) {
-                    data[1].x.push(svf);
-                    data[1].y.push((svfComp.d_100 + svfComp.t_100)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
+                    fastData[1].x.push(svf);
+                    fastData[1].y.push((svfComp.d_100 + svfComp.t_100)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
                 }
                 if (gognSelected[2].checked) {
-                    data[2].x.push(svf);
-                    data[2].y.push((svfComp.d_1000 + svfComp.t_1000)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
+                    fastData[2].x.push(svf);
+                    fastData[2].y.push((svfComp.d_1000 + svfComp.t_1000)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
                 }
                 if (gognSelected[3].checked) {
-                    data[3].x.push(svf);
-                    data[3].y.push((svfComp.d_ljos + svfComp.t_ljos)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
+                    fastData[3].x.push(svf);
+                    fastData[3].y.push((svfComp.d_ljos + svfComp.t_ljos)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
+                }
+                if (gognSelected[4].checked) {
+                    farData[0].x.push(svf);
+                    farData[0].y.push((svfComp.t_talsamband + svfComp.d_talsamband)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
+                }
+                if (gognSelected[5].checked) {
+                    farData[1].x.push(svf);
+                    farData[1].y.push((svfComp.t_hahrada + svfComp.d_hahrada)/(svfComp.thettbyli_fjoldi + svfComp.dreifbyli_fjoldi)*100);
                 }
 
             }
@@ -130,21 +162,44 @@ function sveitarfelog(event) {
 
     let layout = {barmode: 'group'}
 
-    Plotly.newPlot($('#svf-fastanet')[0], data, layout)
+    Plotly.newPlot($('#svf-fastanet')[0], fastData, layout)
+    Plotly.newPlot($('#svf-farnet')[0], farData, layout)
 }
 
 function pieCharts() {
     let pieData = pfsData.filter(x => (x.data != "sveitarfelog"));
-    let pieValues = [];
+    let layout = {
+        grid: {
+            rows: 1,
+            columns:2
+        }
+    };
 
     pieData.forEach(x => {
-        pieValues.push([x.hlutfall_tal*100, 100 - x.hlutfall_tal*100]);
-        pieValues.push([x.hlutfall_hh*100, 100 - x.hlutfall_hh*100]);
+        data = [
+            {
+                values: [x.hlutfall_tal*100, 100 - x.hlutfall_tal*100],
+                name: x.data,
+                type: 'pie',
+                marker: {colors: [blue, green]},
+                domain: {
+                    row: 0,
+                    column: 0
+                }
+            }, {
+                values: [x.hlutfall_hh*100, 100 - x.hlutfall_hh*100],
+                name: x.data,
+                type: 'pie',
+                marker: {colors: [blue, green]},
+                domain: {
+                    row: 0,
+                    column: 1
+                }
+            }
+        ]
+        layout.title = x.data
+        Plotly.newPlot($('#' + x.data)[0], data, layout);
     });
-
-    
-
-    Plotly.newPlot($('#pie-charts')[0], pData, pieLayout);
 }
 
 
@@ -160,3 +215,6 @@ class Trace {
 
     }
 }
+
+sveitarfelog();
+pieCharts();
